5bba83f455333aa53781770861ed5c2b2b06a965
DrunkBelligerentWizard <j.c.vandagrift@gmail.com>
2025-05-11 21:20:03 -0700

    fix: corrected pip install line to work with requirements.txt file

--- raspberry_pi_src/shell_scripts/pi_first_time_setup.sh
+++ raspberry_pi_src/shell_scripts/pi_first_time_setup.sh
@@ -3,7 +3,7 @@
 sudo apt-get install python3-pip
 python3 -m venv ./.venv
 source .venv/bin/activate
-pip install .
+pip install -r requirements.txt
 
 # Configure cron job to automatically launch control system script
 # Path to the script
