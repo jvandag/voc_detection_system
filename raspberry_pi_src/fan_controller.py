79023bc704cca31b6cb50c77a82f59725fdc855a
jvandag <j.c.vandagrift@gmail.com>
2025-05-21 12:14:26 -0700

    feat: added upper and lower threshold for toggling fans to prevent rapid toggling

--- raspberry_pi_src/fan_controller.py
+++ raspberry_pi_src/fan_controller.py
@@ -21,9 +21,12 @@ def get_cpu_temp():
 
 def main():
     # get settings from config file
-    fan_pin       = settings.get("fan_GPIO_pin", 32)
-    temp_thresh   = settings.get("fan_temp_thresh", 0.0)
-    poll_rate     = settings.get("fan_temp_poll_rate", 1)
+    fan_pin     = settings.get("fan_GPIO_pin", 32)
+    on_thresh   = settings.get("fan_on_temp_thresh", 0.0)
+    off_thresh  = settings.get("fan_off_temp_thresh", None)
+    poll_rate   = settings.get("fan_temp_poll_rate", 1)
+    
+    state = "OFF"
 
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BOARD)
@@ -33,12 +36,16 @@ def main():
         while True:
             # Get the temperature of the cpu
             temp = get_cpu_temp()
-            hot = (temp >= temp_thresh)
             
             # toggle fan based off of temp
-            GPIO.output(fan_pin, GPIO.HIGH if hot else GPIO.LOW)
-            state = "ON" if hot else "OFF"
-            print(f"CPU Temp: {temp:.2f}°C, Threshold Temp: {temp_thresh:.1f}°C → Fan {state}")
+            if state == "ON" and off_thresh != None and (temp <= off_thresh):
+                #GPIO.output(fan_pin, GPIO.LOW)
+                state = "OFF"
+            elif state == "OFF" and (temp >= on_thresh):
+                #GPIO.output(fan_pin, GPIO.HIGH)
+                state = "ON"
+                
+            print(f"CPU Temp: {temp:.2f}°C, Threshold Temp: {on_thresh:.1f}/{off_thresh:.1f}°C → Fan {state}")
             time.sleep(poll_rate)
     except KeyboardInterrupt:
         print("\nInterrupted; cleaning up...")
