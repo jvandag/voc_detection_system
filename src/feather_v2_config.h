d55279f3b3a0524c91a2950d30f63cb8dfbe9eb8
DrunkBelligerentWizard <j.c.vandagrift@gmail.com>
2025-10-26 02:37:47 -0700

    feat: moved some configurations to sensor specific headers

--- src/feather_v2_config.h
+++ src/feather_v2_config.h
@@ -1,19 +1,15 @@
 #pragma once
 
-
-#define N_KIT_SENS 8
+#define I2C_FREQ  400000
+#define BAUD_RATE 115200
 #define SD_PIN_CS 33 // Chip Select pin for your SD interface, pin 10 on adafruit esp32-s3, pin 33 on adafruit esp32 V2
-#define PANIC_LED LED_BUILTIN
-#define PANIC_DUR 5
-#define MEAS_DUR 5
-#define SAMPLE_NAME "MEDIUM"
-#define COMPLETE_READ 0xFF
+#define SAMPLE_INTERVAL 1*60*1000 // interval between collecting sensor data in ms
+#define CHAMBER_NAME "1"
 
 // Explicit pin defines for the Adafruit esp32 V2 board
 // Pinouts for this board must be explicitly defined to work with platformio
 #define SDA       22
 #define SCL       20
-#define I2C_FREQ  400000
 #define MOSI      19
 #define MISO      21  
 #define SCK       5
