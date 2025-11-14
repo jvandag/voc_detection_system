fac55869aff373011390b45999e04b929caed122
DrunkBelligerentWizard <j.c.vandagrift@gmail.com>
2025-11-13 23:12:10 -0800

    feat: optimized for fastest possible temp, pressure, and humidity readings.

--- src/test_programs/bme688_test_forced.cc
+++ src/test_programs/bme688_test_forced.cc
@@ -31,8 +31,6 @@ void setup(void)
 	while (!Serial)
 		delay(10);
 		
-	/* initializes the sensor based on SPI library */
-
 	/* Communication interface set for all the 8 sensors in the development kit */
     for (uint8_t i = 0; i < N_KIT_SENS; i++) {
         commSetup[i] = commMuxSetConfig(Wire, SPI, i, commSetup[i]);
@@ -40,7 +38,6 @@ void setup(void)
                     &commSetup[i]);
         if(bme[i].checkStatus()) {
         Serial.println("Initializing sensor " + String(i) + " failed with error " + bme[i].statusString());
-        //   panicLeds();
         }
     }
 
@@ -54,23 +51,36 @@ void setup(void)
         bme[i].setTPH();
 
         /* Set the heater configuration to 300 deg C for 100ms for Forced mode */
-        bme[i].setHeaterProf(100, 10);
+        bme[i].setHeaterProf(0, 0);
     }
 	
 
 	Serial.println("TimeStamp(ms), Temperature(deg C), Pressure(Pa), Humidity(%), Gas resistance(ohm), Status");
 }
 
+bool initial = true;
 void loop(void)
 {
 	bme68xData data[N_KIT_SENS];
-    for (uint8_t i = 0; i < N_KIT_SENS; i++) {
-        bme[i].setOpMode(BME68X_FORCED_MODE);
-        delayMicroseconds(bme[i].getMeasDur());
+    int num_sensors = 3;
 
+    // stagger the start of each reading so they're evenly spaced appart
+    if (initial) {
+        for (uint8_t i = 0; i < num_sensors; i++) {
+            bme[i].setOpMode(BME68X_FORCED_MODE);
+            delayMicroseconds(bme[i].getMeasDur()/num_sensors);
+        }
+        initial = false;
+    }
+
+    // get sensor readings
+    for (uint8_t i = 0; i < num_sensors; i++) {
         if (bme[i].fetchData())
         {
+            bme[i].setOpMode(BME68X_FORCED_MODE);
+            delayMicroseconds(bme[i].getMeasDur()/num_sensors);
             bme[i].getData(data[i]);
+            Serial.print("Sensor: " + String(i) + ", ");
             Serial.print(String(millis()) + ", ");
             Serial.print(String(data[i].temperature) + ", ");
             Serial.print(String(data[i].pressure) + ", ");
