8f15cb605cbc784384e5e45bb40decdccfa6f353
DrunkBelligerentWizard <j.c.vandagrift@gmail.com>
2025-11-22 19:00:19 -0800

    fix: bug that caused scd4x_single_shot_avg returning bad reads

--- src/scd4x.hh
+++ src/scd4x.hh
@@ -18,8 +18,10 @@ static int16_t error;
 
 static int16_t call_and_check_error(int16_t error_status, String caller_identifier) {
     if (error_status != NO_ERROR) {
+        
         errorToString(error_status, errorMessage, sizeof errorMessage);
         Serial.printf("Error trying to execute %s: %s", caller_identifier, errorMessage);
+        Serial.print("##ALERT, " + String(CHAMBER_NAME) + ", SCD4x Error: " + String(errorMessage));
     }
     return error_status;
 }
@@ -45,18 +47,19 @@ void scd4x_init() {
     scd4x.setAutomaticSelfCalibrationEnabled(false);
 }
 
-void scd4x_single_shot_avg(uint16_t &co2_ppm, float &temp_cel, float &rel_hum) {
+bool scd4x_single_shot_avg(uint16_t &co2_ppm, float &temp_cel, float &rel_hum) {
   // Take 3 shots back-to-back and average
   uint16_t c1, c2, c3;
   float t1, t2, t3, h1, h2, h3;
 
   // Discard first reading
-  call_and_check_error(scd4x.measureSingleShot(), "measureSingleShot()");
-
+  scd4x.measureSingleShot();
+  
   call_and_check_error(scd4x.measureAndReadSingleShot(c1, t1, h1), "measureAndReadSingleShot()");
   call_and_check_error(scd4x.measureAndReadSingleShot(c2, t2, h2), "measureAndReadSingleShot()");
   call_and_check_error(scd4x.measureAndReadSingleShot(c3, t3, h3), "measureAndReadSingleShot()");
   co2_ppm  = (c1 + c2 + c3) / 3;
   temp_cel = (t1 + t2 + t3) / 3;
   rel_hum  = (h1 + h2 + h3) / 3;
+  return true;
 }
