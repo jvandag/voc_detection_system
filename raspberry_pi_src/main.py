d2b47682b3c000c141527b7cfb319068198a48d9
jvandag <j.c.vandagrift@gmail.com>
2025-12-06 01:08:57 -0800

    chore: removed unused files left over from refactor

--- raspberry_pi_src/main.py
+++
@@ -1,9 +0,0 @@
-import time
-import RPi.GPIO as GPIO
-from control_sys.ControlSystem import ControlSystem
-from config.config_manager import settings
-
-if __name__ == "__main__":
-    control_system = ControlSystem()
-    control_system.run_sys()
-    
