d5fba47c9ebed3341738c191c6a5a770510eea99
jvandag <j.c.vandagrift@gmail.com>
2025-12-08 18:45:41 -0800

    feat: made main activate the control system

--- raspberry_pi_src/src/pi_src/main.py
+++ raspberry_pi_src/src/pi_src/main.py
@@ -1,18 +1,18 @@
 import time
 import RPi.GPIO as GPIO
-# from control_sys.ControlSystem import ControlSystem
-from .control_sys import LEDStripController
+from .control_sys.ControlSystem import ControlSystem
+from .control_sys.LEDBreather import LEDBreather
 from .config.config_manager import settings
 
 def main() -> int:
-    led_breather = LEDStripController.LEDBreather()
+    led_breather = LEDBreather()
     try:
-        # control_system = ControlSystem()
-        # control_system.run_sys()
-        led_breather.start()
-        while(True): time.sleep(0.2)   # Enable serial mesage monitor
+        control_system = ControlSystem()
+        control_system.run_sys()
+        # led_breather.start()
+        # while(True): time.sleep(0.2)   # Enable serial mesage monitor
         return 0;
     except KeyboardInterrupt:
-        print("SIGTERM %s!\n" % ("Hello World!"))
-        led_breather.stop()
+        print("Keyboard Interrupt, Exiting...")
+        # led_breather.stop()
         return 0;
