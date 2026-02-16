86155f55e8044ad6e4815426195bed617440e225
jvandag <j.c.vandagrift@gmail.com>
2026-02-16 15:11:02 -0800

    chore: adjusted test

--- raspberry_pi_src/src/pi_src/system_test.py
+++ raspberry_pi_src/src/pi_src/system_test.py
@@ -8,10 +8,10 @@ import RPi.GPIO as GPIO
 from .config.config_manager import settings
 
 def main() -> int:
-    print("starting")
+    print("test starting")
     try:
         # Initialize control system
-        control_sys = ControlSystem()
+        control_sys = ControlSystem(flush_mode=True)
     except Exception as e:
         print(f"Error initializing Control System: {e}")
         return 1
@@ -19,7 +19,7 @@ def main() -> int:
     try:
         chamber_list = {    
             "1":            {"group": "Test 1", "slot": 1},
-            "Light Roast":  {"group": "Test 2", "slot": 2},
+            "Light Roast":  {"group": "Test 1", "slot": 2},
             "Medium Roast": {"group": "Test 1", "slot": 3},
             "Dark Roast":   {"group": "Test 1", "slot": 4}
             }
@@ -33,25 +33,25 @@ def main() -> int:
         for name, kwargs in chamber_list.items():
             control_sys.add_chamber(name, **kwargs)
             
-        control_sys.add_chamber("1", "Test 1", 1)
-        control_sys.add_chamber("2", "Test 1", 1)
-        control_sys.add_chamber("1", "Test 1", 6)
+        # control_sys.add_chamber("1", "Test 1", 1)
+        # control_sys.add_chamber("2", "Test 1", 1)
+        # control_sys.add_chamber("1", "Test 1", 6)
         
-        control_sys.turn_vacuum_on();
-        sleep(1)
+        # control_sys.turn_vacuum_on();
+        # sleep(1)
         
-        print("Openning Valves")
-        for chamber in control_sys.chambers.values():
-            control_sys.open_gas_valve(chamber)
-            sleep(0.5)
-            control_sys.open_vacuum_valve(chamber)
-            sleep(0.5)
-            control_sys.close_gas_valve(chamber)
-            sleep(0.5)
-            control_sys.close_vacuum_valve(chamber)
-            sleep(1)
-        control_sys.turn_vacuum_off();
-        sleep(1)
+        # print("Openning Valves")
+        # for chamber in control_sys.chambers.values():
+        #     # control_sys.open_gas_valve(chamber)
+        #     sleep(0.5)
+        #     control_sys.open_vacuum_valve(chamber)
+        #     sleep(0.5)
+        #     control_sys.close_gas_valve(chamber)
+        #     sleep(0.5)
+        #     control_sys.close_vacuum_valve(chamber)
+        #     sleep(1)
+        # control_sys.turn_vacuum_off();
+        # sleep(1)
             
         control_sys.run_sys()
         
